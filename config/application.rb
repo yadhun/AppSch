4a38c09e3b85e619b0d0b53dd645dd3a31ee44c8
shashank <shashank.v.shandilya@gmail.com>
2015-07-13 14:31:39 +0530

    changes made on workspace

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module AppSch
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = fals
+    config.assets.initialize_on_precompile = false
   end
 end
